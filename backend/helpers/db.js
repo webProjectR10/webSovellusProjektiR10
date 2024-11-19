595a24b305e3423fa1aa3cc21655d92b99738a16
JaniKuivila <141952805+JaniKuivila@users.noreply.github.com>
2024-11-19 12:52:01 +0200

    +test.js +test_movie.sql & fixed test order

--- backend/helpers/db.js
+++ backend/helpers/db.js
@@ -10,7 +10,7 @@ const openDb = () => {
     const pool = new Pool({
       user: process.env.DB_USER,
       host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
+      database: process.env.NODE_ENV === 'development' ? process.env.DB_NAME : process.env.TEST_DB_NAME,
       password: process.env.DB_PASSWORD,
       port: process.env.DB_PORT,
     });
