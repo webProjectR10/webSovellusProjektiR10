65e492de3c54195ab4f09d256dadc34dbc4b6751
jjaniju <152972331+jjaniju@users.noreply.github.com>
2024-12-08 16:47:49 +0200

    fix css and  NavBar

--- webproject/src/components/NavBar.js
+++ webproject/src/components/NavBar.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../NavBar.css'; 
+
+
 
 const NavBar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <Link className="nav-link active" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link active" to="/groupsscreen">Groups</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link active" to="/login">Log In</Link>
-          </li>
+    <nav className="navbar">
+      <div className="navdiv">
+
+        <ul>
+        <button><Link to="/">Movies</Link></button>
+        <button><Link to="/about">groups</Link></button>
+        <button><Link to="/contact">schedules</Link></button>
+        </ul>
+        <ul>
+        <button><Link to="/signup">login</Link></button>
         </ul>
       </div>
     </nav>
   );
 };
 
-export default NavBar;  // Default export
-
+export default NavBar;
