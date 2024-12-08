f776ee84513e7709aaf03335c5f887b999449322
jjaniju <152972331+jjaniju@users.noreply.github.com>
2024-12-08 17:18:50 +0200

    2.0

--- webproject/src/components/NavBar.js
+++ webproject/src/components/NavBar.js
@@ -8,15 +8,13 @@ const NavBar = () => {
   return (
     <nav className="navbar">
       <div className="navdiv">
-
+        
         <ul>
-        <button><Link to="/">Movies</Link></button>
-        <button><Link to="/about">groups</Link></button>
-        <button><Link to="/contact">schedules</Link></button>
-        </ul>
-        <ul>
-        <button><Link to="/signup">login</Link></button>
-        </ul>
+          <button><Link to="/">Movies</Link></button>
+          <button><Link to="/about">groups</Link></button>
+          <button><Link to="/contact">schedules</Link></button>
+        </ul> 
+          <button id="login-button"><Link to="/signup">login</Link></button>
       </div>
     </nav>
   );
