ce1f7b575a99eb8cfcf423fe6db1cb899774fbbe
JaniKuivila <141952805+JaniKuivila@users.noreply.github.com>
2024-12-11 18:58:12 +0200

    Delete group button only visible when owner

--- webproject/src/components/GroupDetailsPage.js
+++ webproject/src/components/GroupDetailsPage.js
@@ -27,28 +27,32 @@ const GroupDetailsPage = () => {
     return <p>Loading...</p>;
   }
 
-    const handleDeleteGroup = async (e) => {
-      e.preventDefault();
+  const handleDeleteGroup = async (e) => {
+    e.preventDefault();
 
-      try {
-        const response = await axios.delete(`${process.env.REACT_APP_API_URL}/groups/delete/${groupId}`, {
-          data: { groupid: groupId, ownerid: user.userid  }
-        });
-        navigate('/groupsscreen')
-        console.log('Group deleted successfully:', response.data);
-      } catch(error){
-        console.error('Error deleting group:', error);
-      }
+    try {
+      const response = await axios.delete(`${process.env.REACT_APP_API_URL}/groups/delete/${groupId}`, {
+        data: { groupid: groupId, ownerid: user.userid  }
+      });
+      navigate('/groupsscreen')
+      console.log('Group deleted successfully:', response.data);
+    } catch(error){
+      console.error('Error deleting group:', error);
     }
+  }
+
+  const isOwner = user.userid == groupDetails.ownerid;
 
   return (
     <div className="GroupDetailsPage">
       <h1>{groupDetails.name}</h1>
       <p>Owner: {groupDetails.first_name} {groupDetails.last_name}</p>
       <p>Group ID: {groupDetails.groupid}</p>
-      <button className="btn btn-primary" onClick={handleDeleteGroup}>
+      {isOwner && (
+        <button className="btn btn-primary" onClick={handleDeleteGroup}>
           Delete group
         </button>
+      )}
     </div>
   );
 };
